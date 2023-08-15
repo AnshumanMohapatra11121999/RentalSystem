3638d1d98865717881bc4f498429204283c9b573
bmassioui <bouchaib.massioui@gmail.com>
2023-08-15 10:38:53 +0100

    :fire: Clean up the boilerplate code

---
+++ Server/Controllers/ApiControllerBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BikeRentalSystem.Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public abstract class ApiControllerBase : Controller
+{
+}
