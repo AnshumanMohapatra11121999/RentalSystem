8eb7fd674dd0514dbc9d559a2a22c9545f620768
bmassioui <bouchaib.massioui@gmail.com>
2023-08-15 15:10:29 +0100

    :fire: Changing the way for Tailwind CSS integration

---
+++ Client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './wwwroot/**/*.html',
+        './src/**/*.{razor,html,cshtml}'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
